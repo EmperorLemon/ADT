fa362fdf880e6b0ac2b60ee5a766fc2a64fe1739
Caleb G <cjmeepers@gmail.com>
2022-05-17 13:54:01 +0800

    rename files

---
+++ src/VECTOR_TEST.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "BST.H"
+
+using std::cout;
+using std::ifstream;
+using std::endl;
+
+const std::string TEST_FILE = ".txt";
+
+int main()
+{
+	ifstream file(TEST_FILE.c_str());
+	
+	if (file.bad())
+		return -1;
+	
+	BST<T> bst;
+	
+	return 0;
+}
